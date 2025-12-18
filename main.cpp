2c06dab56677a33b57e4aa26a2f878e10a6781b6
milanacharonova <popelnitckiy2@gmail.com>
2025-12-18 16:17:06 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,12 +6,14 @@
 #include <thread>
 #include <vector>
 #include <algorithm>
+#include <functional>
+#include <sstream>
 
 using namespace std;
 using boost::asio::ip::tcp;
 
+class server;
 class car {
-    std::mutex db_mutex;
     string _brand;
     string _model;
     int _km;
@@ -22,11 +24,9 @@ public:
         : _brand(brand), _model(model), _km(km), _cu(cu) {}
     friend istream& operator>>(istream& in, car& p);
     friend ostream& operator<<(ostream& out, car& p);
-    void display();
-    void find();
-    void add();
-    void del();
-    void redact();
+    void display(shared_ptr<tcp::socket> socket, server* srv);
+    void add(shared_ptr<tcp::socket> socket, server* srv);
+    void del(shared_ptr<tcp::socket> socket, server* srv);
 };
 
 istream& operator>>(istream& in, car& p) {
@@ -39,253 +39,15 @@ ostream& operator<<(ostream& out, car& p) {
     return out;
 }
 
-void car::display() {
-    std::lock_guard<std::mutex> lock(db_mutex);
-    string line;
-    ifstream file("data.txt");
-    if (file.is_open()) {
-        cout << "-------------------------------" << endl;
-        while (getline(file, line)) {
-            cout << line << endl;
-        }
-        file.close();
-        cout << "-------------------------------" << endl;
-        return;
-    }
-    else {
-        cout << "Cant open file" << endl;
-        return;
-    }
-}
-
-void car::find() {
-    std::lock_guard<std::mutex> lock(db_mutex);
-    int flag = 0;
-    ifstream file("data.txt");
-    if (file.is_open()) {
-        int ch;
-        cout << "Choose what to search\n1 - Brand\n2 - Model\n3 - KM\n4 - Current user\n" << endl;
-        cin >> ch;
-        switch (ch) {
-        case 1: {
-            cout << "Type brand:";
-            string brand;
-            cin >> brand;
-            cout << "-------------------------------" << endl;
-            string line;
-            while (file >> _brand >> _model >> _km >> _cu) {
-                if (brand == _brand) {
-                    cout << _brand << " " << _model << " " << _km << " " << _cu << endl;
-                    flag = 1;
-                }
-            }
-            if (flag == 0) {
-                cout << "Nothing found :(" << endl;
-            }
-            cout << "-------------------------------" << endl;
-            file.close();
-            break;
-        }
-        case 2: {
-            cout << "Type model:";
-            string model;
-            cin >> model;
-            cout << "-------------------------------" << endl;
-            string line;
-            while (file >> _brand >> _model >> _km >> _cu) {
-                if (model == _model) {
-                    cout << _brand << " " << _model << " " << _km << " " << _cu << endl;
-                    flag = 1;
-                }
-            }
-            if (flag == 0) {
-                cout << "Nothing found :(" << endl;
-            }
-            cout << "-------------------------------" << endl;
-            file.close();
-            break;
-        }
-        case 3: {
-            cout << "Type KM:";
-            int KM;
-            cin >> KM;
-            cout << "-------------------------------" << endl;
-            string line;
-            while (file >> _brand >> _model >> _km >> _cu) {
-                if (KM == _km) {
-                    cout << _brand << " " << _model << " " << _km << " " << _cu << endl;
-                    flag = 1;
-                }
-            }
-            if (flag == 0) {
-                cout << "Nothing found :(" << endl;
-            }
-            cout << "-------------------------------" << endl;
-            file.close();
-            break;
-        }
-        case 4: {
-            cout << "Type Current User:";
-            string cu;
-            cin >> cu;
-            cout << "-------------------------------" << endl;
-            string line;
-            while (file >> _brand >> _model >> _km >> _cu) {
-                if (cu == _cu) {
-                    cout << _brand << " " << _model << " " << _km << " " << _cu << endl;
-                    flag = 1;
-                }
-            }
-            if (flag == 0) {
-                cout << "Nothing found :(" << endl;
-            }
-            cout << "-------------------------------" << endl;
-            file.close();
-            break;
-        }
-        default: {
-            break;
-        }
-        }
-    }
-    else {
-        cout << "Cant open file";
-    }
-}
-
-void car::add() {
-    std::lock_guard<std::mutex> lock(db_mutex);
-    cout << "Enter brand of car: ";
-    cin >> _brand;
-    cout << "Enter model of car: ";
-    cin >> _model;
-    cout << "Enter how many km did car ride: ";
-    cin >> _km;
-    cout << "Enter current user of car: ";
-    cin >> _cu;
-    ofstream file("data.txt", ios::app);
-    if (file.is_open()) {
-        file << _brand << " " << _model << " " << _km << " " << _cu << endl;
-    }
-    file.close();
-}
-
-void car::del() {
-    std::lock_guard<std::mutex> lock(db_mutex);
-    int count = 0;
-    ifstream file("data.txt");
-    if (file.is_open()) {
-        while (file >> _brand >> _model >> _km >> _cu) {
-            count++;
-        }
-    }
-    else {
-        cout << "Cant open file :(" << endl;
-        return;
-    }
-    car* arr = new car[count];
-    cout << "-------------------------------" << endl;
-    ifstream f("data.txt");
-    if (f.is_open()) {
-        for (int i = 0; i < count; i++) {
-            f >> arr[i]._brand >> arr[i]._model >> arr[i]._km >> arr[i]._cu;
-            cout << i << ")" << " " << arr[i]._brand << " " << arr[i]._model << " " << arr[i]._km << " " << arr[i]._cu << endl;
-        }
-    }
-    else {
-        cout << "Cant open file :(" << endl;
-        delete[] arr;
-        return;
-    }
-    f.close();
-    file.close();
-    cout << "-------------------------------" << endl;
-    cout << "Enter number of line u want to del: ";
-    int num;
-    cin >> num;
-    ofstream file1("data.txt");
-    if (file1.is_open()) {
-        for (int i = 0; i < count; i++) {
-            if (i == num) {
-            }
-            else {
-                file1 << arr[i]._brand << " " << arr[i]._model << " " << arr[i]._km << " " << arr[i]._cu << endl;
-            }
-        }
-    }
-    else {
-        cout << "Cant open file :(" << endl;
-    }
-    file1.close();
-    delete[] arr;
-}
-
-void car::redact() {
-    std::lock_guard<std::mutex> lock(db_mutex);
-    int count = 0;
-    ifstream file("data.txt");
-    if (file.is_open()) {
-        while (file >> _brand >> _model >> _km >> _cu) {
-            count++;
-        }
-    }
-    else {
-        cout << "Cant open file :(" << endl;
-        return;
-    }
-    car* arr = new car[count];
-    cout << "-------------------------------" << endl;
-    ifstream f("data.txt");
-    if (f.is_open()) {
-        for (int i = 0; i < count; i++) {
-            f >> arr[i]._brand >> arr[i]._model >> arr[i]._km >> arr[i]._cu;
-            cout << i << ")" << " " << arr[i]._brand << " " << arr[i]._model << " " << arr[i]._km << " " << arr[i]._cu << endl;
-        }
-    }
-    else {
-        cout << "Cant open file :(" << endl;
-        delete[] arr;
-        return;
-    }
-    f.close();
-    file.close();
-    cout << "-------------------------------" << endl;
-    cout << "Enter number of line u want to edit: ";
-    int num;
-    cin >> num;
-    cout << "Enter brand of car: ";
-    cin >> _brand;
-    cout << "Enter model of car: ";
-    cin >> _model;
-    cout << "Enter how many km did car ride: ";
-    cin >> _km;
-    cout << "Enter current user of car: ";
-    cin >> _cu;
-    ofstream file1("data.txt");
-    if (file1.is_open()) {
-        for (int i = 0; i < count; i++) {
-            if (i == num) {
-                file1 << _brand << " " << _model << " " << _km << " " << _cu << endl;
-            }
-            else {
-                file1 << arr[i]._brand << " " << arr[i]._model << " " << arr[i]._km << " " << arr[i]._cu << endl;
-            }
-        }
-    }
-    else {
-        cout << "Cant open file :(" << endl;
-    }
-    file1.close();
-    delete[] arr;
-}
-
 class server {
 private:
     boost::asio::io_context io_context;
     tcp::acceptor acceptor;
     vector<shared_ptr<tcp::socket>> clients;
-    mutex clients_mutex;
+    // Добавлен мьютекс для защиты операций с базой данных
 public:
+    mutex clients_mutex;
+    mutex db_mutex;
     server(uint16_t port) : acceptor(io_context, tcp::endpoint(tcp::v4(), port)) {
         cout << "Сервер запущен на порту:" << port << endl;
         cout << "-------------------------------" << endl;
@@ -294,20 +56,31 @@ public:
 
     void accept_connections() {
         auto socket = make_shared<tcp::socket>(io_context);
-         acceptor.async_accept(*socket, [this, socket](const boost::system::error_code& ec) {
+        acceptor.async_accept(*socket, [this, socket](const boost::system::error_code& ec) {
             if (!ec) {
                 cout << "Новое подключение:" << socket->remote_endpoint() << endl;
                 {
                     lock_guard<mutex> lock(clients_mutex);
                     clients.push_back(socket);
                 }
-                string menu = "----------menu-----------\n";
-                boost::asio::async_write(*socket, boost::asio::buffer(menu),
-                    [](const boost::system::error_code&, size_t) {});
+                show_menu(socket);
                 user_input(socket);
             }
             accept_connections();
         });
+    }
+
+    void show_menu(shared_ptr<tcp::socket> socket) {
+        string menu = "----------menu-----------\n"
+                      "1 - Display all cars\n"
+                      "2 - Add car\n"
+                      "3 - Delete car\n"
+                      "4 - Edit car\n"
+                      "6 - Exit\n"
+                      "-------------------------\n"
+                      "Enter your choice: ";
+        boost::asio::async_write(*socket, boost::asio::buffer(menu),
+            [](const boost::system::error_code&, size_t) {});
     }
 
     void user_input(shared_ptr<tcp::socket> socket) {
@@ -323,32 +96,31 @@ public:
                     cout << "[" << socket->remote_endpoint() << "] " << input << endl;
                     car taxi;
                     if (input == "1") {
-                        taxi.display();
+                        taxi.display(socket, this);
                     }
                     else if (input == "2") {
-                        taxi.find();
+                        taxi.add(socket, this);
                     }
                     else if (input == "3") {
-                        taxi.add();
-                    }
-                    else if (input == "4") {
-                        taxi.del();
-                    }
-                    else if (input == "5") {
-                        taxi.redact();
+                        taxi.del(socket, this);
                     }
                     else if (input == "6") {
-                        boost::system::error_code close_ec;
-                        socket->close(close_ec);
+                        string goodbye = "Goodbye!\n";
+                        boost::asio::async_write(*socket, boost::asio::buffer(goodbye),
+                            [socket](const boost::system::error_code&, size_t) {
+                                boost::system::error_code close_ec;
+                                socket->close(close_ec);
+                            });
                         return;
                     }
                     else {
-                        string error = "Unknown command\n";
+                        string error = "Unknown command. Try again:\n";
                         boost::asio::async_write(*socket, boost::asio::buffer(error),
-                            [](const boost::system::error_code&, size_t) {});
+                            [this, socket](const boost::system::error_code&, size_t) {
+                                this->show_menu(socket);
+                            });
                     }
                 }
-                this->user_input(socket);
             }
             else {
                 cout << "Клиент отключился:" << socket->remote_endpoint() << endl;
@@ -365,7 +137,172 @@ public:
     void run() {
         this->io_context.run();
     }
+
+    void ip_input(shared_ptr<tcp::socket> socket, function<void(string)> callback) {
+        auto buffer = make_shared<string>(1024, '\0');
+        socket->async_read_some(boost::asio::buffer(*buffer),
+            [this, socket, buffer, callback](const boost::system::error_code& ec, size_t bytes_transferred) {
+                if (!ec && bytes_transferred > 0) {
+                    buffer->resize(bytes_transferred);
+                    while (!buffer->empty() && (buffer->back() == '\n' || buffer->back() == '\r')) {
+                        buffer->pop_back();
+                    }
+                    if (!buffer->empty()) {
+                        callback(*buffer);
+                    } else {
+                        this->ip_input(socket, callback);
+                    }
+                } else {
+                    boost::system::error_code close_ec;
+                    socket->close(close_ec);
+                }
+            });
+    }
 };
+
+void car::display(shared_ptr<tcp::socket> socket, server* srv) {
+    // Используем мьютекс сервера для защиты доступа к файлу
+    std::lock_guard<std::mutex> lock(srv->db_mutex);
+    string response = "-------------------------------\n";
+    ifstream file("data.txt");
+
+    if (file.is_open()) {
+        string line;
+        bool found = false;
+        while (getline(file, line)) {
+            response += line + "\n";
+            found = true;
+        }
+        file.close();
+
+        if (!found) {
+            response += "Database is empty\n";
+        }
+    } else {
+        response = "Error: Can't open file\n";
+    }
+
+    response += "-------------------------------\n";
+
+    boost::asio::async_write(*socket, boost::asio::buffer(response),
+        [srv, socket](const boost::system::error_code&, size_t) {
+            srv->show_menu(socket);
+        });
+}
+
+void car::add(shared_ptr<tcp::socket> socket, server* srv) {
+    string prompt = "Enter brand model km current_user (separated by spaces):\n";
+    boost::asio::async_write(*socket, boost::asio::buffer(prompt),
+        [this, socket, srv](const boost::system::error_code& ec, size_t) {
+            if (!ec) {
+                srv->ip_input(socket, [this, socket, srv](const string& input) {
+                    istringstream iss(input);
+                    string brand, model, cu;
+                    int km;
+
+                    if (iss >> brand >> model >> km >> cu) {
+                        {
+                            // Используем мьютекс сервера для защиты записи в файл
+                            std::lock_guard<std::mutex> lock(srv->db_mutex);
+                            ofstream file("data.txt", ios::app);
+                            if (file.is_open()) {
+                                file << brand << " " << model << " " << km << " " << cu << endl;
+                                file.close();
+                            }
+                        }
+                        string response = "Car added successfully!\n";
+                        boost::asio::async_write(*socket, boost::asio::buffer(response),
+                            [srv, socket](const boost::system::error_code&, size_t) {
+                                srv->show_menu(socket);
+                            });
+                    } else {
+                        string error = "Invalid format. Use: brand model km user\n";
+                        boost::asio::async_write(*socket, boost::asio::buffer(error),
+                            [this, socket, srv](const boost::system::error_code&, size_t) {
+                                this->add(socket, srv);
+                            });
+                    }
+                });
+            }
+        });
+}
+
+void car::del(shared_ptr<tcp::socket> socket, server* srv) {
+    // Используем мьютекс сервера для защиты чтения файла
+    std::lock_guard<std::mutex> lock(srv->db_mutex);
+    ifstream file("data.txt");
+    vector<car> cars;
+
+    if (file.is_open()) {
+        string brand, model, cu;
+        int km;
+        while (file >> brand >> model >> km >> cu) {
+            cars.push_back(car(brand, model, km, cu));
+        }
+        file.close();
+    }
+
+    if (cars.empty()) {
+        string response = "Database is empty. Nothing to delete.\n";
+        boost::asio::async_write(*socket, boost::asio::buffer(response),
+            [srv, socket](const boost::system::error_code&, size_t) {
+                srv->show_menu(socket);
+            });
+        return;
+    }
+
+    // Показываем список машин
+    string list = "-------------------------------\n";
+    for (size_t i = 0; i < cars.size(); i++) {
+        list += to_string(i) + ") " + cars[i]._brand + " " + cars[i]._model +
+                " " + to_string(cars[i]._km) + " " + cars[i]._cu + "\n";
+    }
+    list += "-------------------------------\n";
+    list += "Enter number of car to delete: ";
+
+    boost::asio::async_write(*socket, boost::asio::buffer(list),
+        [this, socket, srv, cars](const boost::system::error_code& ec, size_t) {
+            if (!ec) {
+                srv->ip_input(socket, [this, socket, srv, cars](const string& input) {
+                    try {
+                        size_t num = stoul(input);
+                        if (num >= cars.size()) {
+                            string error = "Invalid number. Try again:\n";
+                            boost::asio::async_write(*socket, boost::asio::buffer(error),
+                                [this, socket, srv, cars](const boost::system::error_code&, size_t) {
+                                    this->del(socket, srv); // Перезапускаем удаление
+                                });
+                            return;
+                        }
+
+                        {
+                            // Используем мьютекс сервера для защиты записи в файл
+                            std::lock_guard<std::mutex> lock(srv->db_mutex);
+                            ofstream file("data.txt");
+                            for (size_t i = 0; i < cars.size(); i++) {
+                                if (i != num) {
+                                    file << cars[i]._brand << " " << cars[i]._model << " "
+                                         << cars[i]._km << " " << cars[i]._cu << endl;
+                                }
+                            }
+                        }
+
+                        string response = "Car deleted successfully!\n";
+                        boost::asio::async_write(*socket, boost::asio::buffer(response),
+                            [srv, socket](const boost::system::error_code&, size_t) {
+                                srv->show_menu(socket);
+                            });
+                    } catch (...) {
+                        string error = "Invalid input. Enter a number:\n";
+                        boost::asio::async_write(*socket, boost::asio::buffer(error),
+                            [this, socket, srv, cars](const boost::system::error_code&, size_t) {
+                                this->del(socket, srv); // Перезапускаем удаление
+                            });
+                    }
+                });
+            }
+        });
+}
 
 int main() {
     try {
